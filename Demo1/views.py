88171924431f73a3bb701cb5702be83a27fbdd75
huzefa <shaikhhuzefa76@gmail.com>
2020-04-20 19:32:43 +0530

    Initial commit

---
+++ Demo1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,"/Users/Huzefa/PycharmProjects/djangoresume/Testing/Demo1/templates/index.html")
